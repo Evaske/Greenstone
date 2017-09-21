020d9733d6f5d58cf1542ff2a838fcea76eb585e
Kieran Eves <keyeves@gmail.com>
2017-09-21 08:24:41 -0400

    Added slider overlay

--- site/web/app/themes/GreenstoneStarling/resources/views/front-page.blade.php
+++ site/web/app/themes/GreenstoneStarling/resources/views/front-page.blade.php
@@ -14,6 +14,7 @@
                 @php echo the_post_thumbnail_url('full'); @endphp 2880w"
         style="width:100%;">
         <div class="header-wrapper">
+          <div class="overlay"></div>
           <div class="header">
             <h1>@php echo get_post_meta( get_the_ID(), 'hero_text', true ); @endphp</h1>
             <a href="#" class="button button--purple js-button-scroll" data-scrollto="home-what-we-do">Find Out More</a>
